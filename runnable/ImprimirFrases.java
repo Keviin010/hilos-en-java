db359362809f9d1c47146ee4e184c712885ed94d
keviin <140296481+Keviin010@users.noreply.github.com>
2026-01-16 20:18:50 -0500

    Agregar proyecto de programación con hilos

---
+++ runnable/ImprimirFrases.java
@@ -0,0 +1,17 @@
+package org.hilos.app.runnable;
+
+import static org.hilos.app.SincronizacionThread.imprimirFrases;
+
+public class ImprimirFrases implements Runnable{
+     private String frase1, frase2;
+
+    public ImprimirFrases(String frase1, String frase2) {
+        this.frase1 = frase1;
+        this.frase2 = frase2;
+    }
+
+    @Override
+    public void run() {
+        imprimirFrases(this.frase1, this.frase2);
+    }
+}
