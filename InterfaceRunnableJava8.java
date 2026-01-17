338b536b86e18128f4549179155148f375aa32ec
keviin <140296481+Keviin010@users.noreply.github.com>
2026-01-16 20:19:47 -0500

    Agregar proyecto de programación con java8

---
+++ InterfaceRunnableJava8.java
@@ -0,0 +1,38 @@
+package org.hilos.app;
+
+public class InterfaceRunnableJava8 {
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread main = Thread.currentThread();
+        Runnable viaje = () -> {
+            for (int i = 0; i < 10; i++) {
+                System.out.println(i + "-" + Thread.currentThread().getName());
+                try {
+                    Thread.sleep((long) (Math.random() * 1000));
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            System.out.println("Finalmente me voy de viaje a: " + Thread.currentThread().getName());
+            System.out.println(main.getState());
+        };
+
+        Thread v1 = new Thread(viaje, "Canada");
+        Thread v2 = new Thread(viaje, "Japón");
+        Thread v3 = new Thread(viaje, "Alemania");
+        Thread v4 = new Thread(viaje, "Brasil");
+
+        v1.start();
+        v2.start();
+        v3.start();
+        v4.start();
+
+        v1.join();
+        v2.join();
+        v3.join();
+        v4.join();
+
+        //Thread.sleep(1000);
+        System.out.println("Continuando con la ejecución del método main: " + main.getName());
+    }
+}
